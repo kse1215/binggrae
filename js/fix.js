d835ac59ede35b5d30a500fa027558e4b0926720
Kim Seoeun <kseoeun1215@gmail.com>
2025-08-27 01:57:39 +0900

    fix 버튼 작업

---
+++ js/fix.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function () {
+  //   스크롤 이벤트
+  const topBtn = this.document.querySelector(".top-btn");
+  const downBtn = this.document.querySelector(".dwon-btn");
+  this.window.addEventListener("scroll", function () {
+    // console.log("스크롤시작");
+    if (this.document.documentElement.scrollTop > 200) {
+      topBtn.style.display = "block";
+      downBtn.style.display = "none";
+    } else {
+      topBtn.style.display = "none";
+      downBtn.style.display = "block";
+    }
+  });
+  topBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
