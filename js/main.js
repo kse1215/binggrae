af19bd968fe588a1a3e0487b37bd0f5476763abc
kse1215 <kseoeun1215@gmail.com>
2025-08-26 15:57:21 +0900

    헤더 완료

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 window.addEventListener("load", function () {
+  // 상단 top
   // 언어 메뉴 토글
   const langTxt = this.document.querySelector(".lang_txt");
   const langList = this.document.querySelector(".lang-list");
   langTxt.addEventListener("click", function () {
     langList.classList.toggle("toggleOpen");
+  });
+
+  // 상단 bottom
+  // gnb 메뉴 기능
+  const nav = document.querySelector(".gnb");
+  const headerBg = this.document.querySelector(".header-bg");
+  const depth2Bg = document.querySelector(".depth2-bg");
+  const headerBT = this.document.querySelector(".header_bottom")
+  // nav에 마우스 엔터 했을때 즉 호버했을때
+  nav.addEventListener("mouseenter", function () {
+    headerBg.classList.add("active");
+    depth2Bg.classList.add("active");
+    headerBT.classList.add("active")
+  });
+  // nav에 마우스 리브 했을때 즉 호버를 안했을때
+  nav.addEventListener("mouseleave", function () {
+    headerBg.classList.remove("active");
+    depth2Bg.classList.remove("active");
   });
   // 햄버거 메뉴 기능
   const hamburger = this.document.querySelector("#hamburger");
@@ -11,6 +30,7 @@ window.addEventListener("load", function () {
   hamburger.addEventListener("click", function () {
     hamburger.classList.add("active");
   });
+
   // 카테고리 스와이퍼
   const kSlider = new Swiper(".kategorie_slider", {
     slidesPerView: 1,
@@ -24,7 +44,7 @@ window.addEventListener("load", function () {
       prevEl: ".kategorie .swiper-button-prev",
     },
     breakpoints: {
-        390: {
+      390: {
         slidesPerView: 2,
         // spaceBetween: 4,
       },
@@ -55,7 +75,7 @@ window.addEventListener("load", function () {
       prevEl: ".brand-shop .swiper-button-prev",
     },
     breakpoints: {
-        390: {
+      390: {
         slidesPerView: 2,
         // spaceBetween: 4,
       },
