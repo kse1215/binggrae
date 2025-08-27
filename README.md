5c36b7625c62098185109d53a11b102388c04295
kse1215 <kseoeun1215@gmail.com>
2025-08-27 12:03:53 +0900

    모달창 작업 완료, 사이드 메뉴 작업 시작

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ## font-family: "Noto Sans KR", sans-serif;
 
 ## fix.css .chat-btn:hover span 에서 span에 호버해도 뜨는 오류 발생
+## ㄴ visibility: hidden; 과 visibility: visible; 으로 해결 가능
 ## fix.js downBtn 문제 해결
--- js/fix.js
+++ js/fix.js
@@ -1,4 +1,10 @@
 window.addEventListener("load", function () {
+  // 모달창 닫기
+  const modal = this.document.querySelector(".modal-wrap");
+  const modalClose = this.document.querySelector(".modal-close");
+  modalClose.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
   //   스크롤 이벤트
   const topBtn = this.document.querySelector(".top-btn");
   const downBtn = this.document.querySelector(".dwon-btn");
